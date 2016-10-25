583a6f126051b593cdcdedb4dfbc6f63be178206
David Martin <david@uvd.co.uk>
2016-10-25 23:19:16 +0100

    refactoring

--- src/simple_fsm/core.clj
+++ src/simple_fsm/core.clj
@@ -8,18 +8,21 @@
 (defn kill-character [scene to-kill]
   (update-in scene [:characters to-kill :alive] (constantly false)))
 
-(defn act-on [scene [action [from-character to-character]]]
-  (if (= action :say-what)
-    [scene [[:shoot [to-character from-character]]]]
-    (if (= action :shoot)
-      [(kill-character scene to-character) []]
-      [scene []])))
+(defn get-actions [scene [action [from-character to-character]]]
+  (case action
+        :say-what [[:shoot [to-character from-character]]]
+        []))
+
+(defn update-scene [scene [action [from-character to-character]]]
+  (case action
+    :shoot (kill-character scene to-character)
+    scene))
 
 (defn play-scene [scene [current-action & rest-actions]]
   (println current-action)
   (if (nil? current-action)
     scene
-    (let [[updated-scene next-actions] (act-on scene current-action)]
+    (let [[updated-scene next-actions] ((juxt update-scene get-actions) scene current-action)]
       (recur updated-scene (concat rest-actions next-actions)))))
 
 (play-scene initial-scene [[:say-what ["Brett" "Jules"]]])
